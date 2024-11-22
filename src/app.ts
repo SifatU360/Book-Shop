26a63e1e58a31375a132bdf26bbd1ba3108fa82e
SifatU360 <sifatullah30.info@gmail.com>
2024-11-22 21:59:27 +0600

    Initial Setup Complete

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from 'express'
+
+const app = express();
+
+app.get('/', (req:Request, res:Response) => {
+   res.send({
+    status: true,
+    message:"Server Live"
+   })
+})
+
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import app from "./app";
+import dotenv from "./config";
+import config from "./config";
+
+
+async function server(){
+    try{
+        await mongoose.connect(config.database_url as string)
+
+    app.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port}`);
+    })
+   } catch(err){
+    console.log(err);
+   }
+}
+
+
+
+server();
